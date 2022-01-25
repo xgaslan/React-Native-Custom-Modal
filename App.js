de7e0dcd9a9e84d8e08a911821b38da98976da0f
xgaslan <test@test.com>
2022-01-25 13:21:16 +0300

    React native custom modal

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import
+{
+  View,
+  StyleSheet,
+  Button,
+  Modal,
+  Image,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
 
-export default function App() {
+const ModalPoup = ({ visible, children }) =>
+{
+  const [showModal, setShowModal] = React.useState(visible);
+  const scaleValue = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() =>
+  {
+    toggleModal();
+  }, [visible]);
+  const toggleModal = () =>
+  {
+    if (visible)
+    {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else
+    {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Modal transparent visible={ showModal }>
+      <View style={ styles.modalBackGround }>
+        <Animated.View
+          style={ [styles.modalContainer, { transform: [{ scale: scaleValue }] }] }>
+          { children }
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+const App = () =>
+{
+  const [visible, setVisible] = React.useState(false);
+  return (
+    <View style={ { flex: 1, justifyContent: "center", alignItems: "center" } }>
+      <ModalPoup visible={ visible }>
+        <View style={ { alignItems: "center" } }>
+          <View style={ styles.header }>
+            <TouchableOpacity onPress={ () => setVisible(false) }>
+              <Image
+                source={ require("./assets/x.png") }
+                style={ { height: 30, width: 30 } }
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={ { alignItems: "center" } }>
+          <Image
+            source={ require("./assets/document_1.png") }
+            style={ { height: "95%", width: "100%", marginVertical: 10 } }
+            resizeMode="contain"
+          />
+        </View>
+      </ModalPoup>
+      <Button title="Open Modal" onPress={ () => setVisible(true) } />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  modalBackGround: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    height: "80%",
+    width: "80%",
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  header: {
+    width: "100%",
+    height: 40,
+    alignItems: "flex-end",
+    justifyContent: "center",
   },
 });
+
+export default App;
